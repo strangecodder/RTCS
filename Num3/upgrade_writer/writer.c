21fdd52a46bf6e391c8c45983ae3101579528a4a
strangecodder <strangecodder@gmail.com>
2023-11-21 15:21:07 +0300

    Допилина третья практическая

--- Num3/upgrade_writer/writer.c
+++ Num3/upgrade_writer/writer.c
@@ -5,24 +5,21 @@
 #include <string.h>
 #include <unistd.h>
 
-//Пока не доделал, при записи в файл выдаёт не понятно что
 int main(){
     int fd;
-    char *message = (char*) malloc(sizeof(char) * 256);
-    char *name = (char*) malloc(sizeof(char)*50);
+    char *message = (char*) malloc(sizeof(int) * 2);
+    char *name = (char*) malloc(sizeof(int)* 2);
+
+
     printf("Введите название файла: \n");
-    scanf("%s",name);
+    scanf("%[^\n]%*c",name);
     printf("Введите сообщение, которое хотите записать: \n");
-    scanf("%s",message);
-    size_t size_m = strlen(message);
-    size_t size_n = strlen(name);
-    message = (char*) realloc(message,sizeof(int) * size_m);
-    name = (char*) realloc(name,sizeof(int) * size_n);
-    //int length = strlen(message);
-    printf("%d %d",size_m,size_n);
+    scanf("%[^\n]%*c",message);
+
     fd=open(name, O_CREAT|O_RDWR|O_TRUNC,0666);
-    write(fd,message,256);
+
+    int size_m = strlen(message);
+    write(fd,message,size_m);
+
     printf("Запись в файл прошла успешно\n");
-    //close(fd);
-    //exit(0);
 }
